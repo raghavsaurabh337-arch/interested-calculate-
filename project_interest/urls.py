c70c8cfc50db793aedce395409dfe7eeafbc6588
raghavsaurabh337-arch <raghavsaurabh337@gmail.com>
2026-06-03 22:07:41 +0530

    test APIs / create Apis

--- project_interest/urls.py
+++ project_interest/urls.py
@@ -15,9 +15,11 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
-from django.urls import path
-from  project_interest import views
+from django.urls import path, include
+from  project_interest import views 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('',views.home,name='home'),
+    path('',include('app_interest.urls')),
+    
 ]
