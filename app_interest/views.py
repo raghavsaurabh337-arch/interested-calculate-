b4341226cba7d581058df434e7c33a9fc1bc11e9
raghavsaurabh337-arch <raghavsaurabh337@gmail.com>
2026-06-04 23:44:04 +0530

    APIs create ya test APIs

--- app_interest/views.py
+++ app_interest/views.py
@@ -5,8 +5,33 @@ from rest_framework.response import Response
 # Create your views here.
 @api_view()
 def home(request):
-     return Response({
-          "status":200,
-          'rate':300,
-          'masg':'django python fremworks'
-     })
+     if request.method=='GET':
+          return Response({
+               'status':200,
+               'rate':300,
+               'masg':'django python fremworks',
+               'method':'you called GET Methos',
+          })
+     elif request.method=='POST':
+            return Response({
+               'status':200,
+               'rate':300,
+               'masg':'django python fremworks',
+               'method':'you called POST Methos',
+          })
+     elif request.methos=='PATCH':
+             return Response({
+               "status":200,
+               'rate':300,
+               'masg':'django python fremworks',
+               'method':'you called PATCH Methos',
+          })
+     else:
+             return Response({
+               "status":200,
+               'rate':300,
+               'masg':'django python fremworks',
+               'method':'you called invaild Methos',
+          })
+     
+
